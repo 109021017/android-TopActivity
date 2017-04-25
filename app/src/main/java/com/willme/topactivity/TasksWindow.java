1171d6d03a8fd6c8c06c161dcdc502108221a932
Frieder Bluemle <frieder.bluemle@gmail.com>
2017-04-26 04:00:07 +0800

    Fix whitespace errors and indentation
    This replaces all tab characters with four spaces. It also adds the
    latest default ProGuard template file.

--- app/src/main/java/com/willme/topactivity/TasksWindow.java
+++ app/src/main/java/com/willme/topactivity/TasksWindow.java
@@ -11,43 +11,43 @@ import android.widget.TextView;
 
 public class TasksWindow {
 
-	private static WindowManager.LayoutParams sWindowParams;
-	private static WindowManager sWindowManager;
-	private static View sView;
+    private static WindowManager.LayoutParams sWindowParams;
+    private static WindowManager sWindowManager;
+    private static View sView;
 
-	public static void init(final Context context) {
-		sWindowManager = (WindowManager) context.getApplicationContext()
-				.getSystemService(Context.WINDOW_SERVICE);
+    public static void init(final Context context) {
+        sWindowManager = (WindowManager) context.getApplicationContext()
+                .getSystemService(Context.WINDOW_SERVICE);
 
-		sWindowParams = new WindowManager.LayoutParams(
-				WindowManager.LayoutParams.WRAP_CONTENT,
-				WindowManager.LayoutParams.WRAP_CONTENT,
-				Build.VERSION.SDK_INT <= Build.VERSION_CODES.N ?
-				WindowManager.LayoutParams.TYPE_TOAST: WindowManager.LayoutParams.TYPE_PHONE, 0x18,
-				PixelFormat.TRANSLUCENT);
-		sWindowParams.gravity = Gravity.LEFT + Gravity.TOP;
-		sView = LayoutInflater.from(context).inflate(R.layout.window_tasks,
-				null);
-	}
+        sWindowParams = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                Build.VERSION.SDK_INT <= Build.VERSION_CODES.N ?
+                WindowManager.LayoutParams.TYPE_TOAST: WindowManager.LayoutParams.TYPE_PHONE, 0x18,
+                PixelFormat.TRANSLUCENT);
+        sWindowParams.gravity = Gravity.LEFT + Gravity.TOP;
+        sView = LayoutInflater.from(context).inflate(R.layout.window_tasks,
+                null);
+    }
 
-	public static void show(Context context, final String text) {
-		if (sWindowManager == null) {
-			init(context);
-		}
-		TextView textView = (TextView) sView.findViewById(R.id.text);
-		textView.setText(text);
-		try {
-			sWindowManager.addView(sView, sWindowParams);
-		} catch (Exception e) {}
-		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
-			QuickSettingTileService.updateTile(context);
-	}
+    public static void show(Context context, final String text) {
+        if (sWindowManager == null) {
+            init(context);
+        }
+        TextView textView = (TextView) sView.findViewById(R.id.text);
+        textView.setText(text);
+        try {
+            sWindowManager.addView(sView, sWindowParams);
+        } catch (Exception e) {}
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            QuickSettingTileService.updateTile(context);
+    }
 
-	public static void dismiss(Context context) {
-		try {
-			sWindowManager.removeView(sView);
-		} catch (Exception e) {}
-		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
-			QuickSettingTileService.updateTile(context);
-	}
+    public static void dismiss(Context context) {
+        try {
+            sWindowManager.removeView(sView);
+        } catch (Exception e) {}
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            QuickSettingTileService.updateTile(context);
+    }
 }
