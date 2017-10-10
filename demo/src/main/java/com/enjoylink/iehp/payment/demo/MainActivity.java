fdc48cd3f637681bfe0b18f874f0b2ec70dfad92
panda8 <panda8@126.com>
2017-10-10 09:41:56 +0800

    查看代码： 1. 修改了布局文件查看 运行时布局是哪个。

---
+++ demo/src/main/java/com/enjoylink/iehp/payment/demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.enjoylink.iehp.payment.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
